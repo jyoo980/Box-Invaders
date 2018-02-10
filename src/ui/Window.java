f2966157c5da6828856e557f37e2e5ec6de03b5e
James Yoo <jyoo980@gmail.com>
2018-02-10 13:16:17 -0800

    Finished refactor of Window.java

--- src/ui/Window.java
+++ src/ui/Window.java
@@ -6,11 +6,10 @@ import java.awt.*;
 public class Window extends Canvas {
     private static final long serialVersionUID = -2408460053372835L;
 
-    private Window(int width, int height, String title, Game game) {
-        JFrame window = new JFrame(title);      //basically the frame of our game
+    public Window(int width, int height, String title, Game game) {
+        JFrame window = new JFrame(title);
         setWindowDimensions(window, width, height);
         setWindowBehaviour(window);
-
         addWindowStartGame(window, game);
     }
 
@@ -33,52 +32,38 @@ public class Window extends Canvas {
         game.start();
     }
 
-
     public static class HeadUpDisplay {
-        public static int HEALTH = 100;     //this is the initial health that we start off with
+        public static int HEALTH = 100;
+        public static int greenValue = 255;
+        private static final int MAX_GREEN_VAL = 255;
+        private int score;
+        private int level;
 
-        public static int greenValue = 255; //as this gradually decreases, so will our green colour intensity
-                                            // we'll take care of this in our collision() method in the Player class
-
-        public int score = 0;       //current score
-        public int level = 1;       //the level we're at right now
 
         public HeadUpDisplay() {
-            this.score = 0;
-            this.level = 1;
+            score = 0;
+            level = 1;
         }
 
-
-
         public void tick(){
-            Game.clamp(this.HEALTH, 0, HEALTH);
-            Game.clamp(greenValue, 0, 255);
-
+            clampGameValues();
             score++;
         }
 
-        public void setScore(int score) {
-            this.score = score;
-
+        private void clampGameValues() {
+            Game.clamp(HEALTH, 0, HEALTH);
+            Game.clamp(greenValue, 0, MAX_GREEN_VAL);
         }
 
         public void setLevel(int level) {
             this.level = level;
-
         }
-
-        public int getScore() {
-            return this.score;
-
-        }
-
 
         public int getLevel() {
-            return this.level;
-
+            return level;
         }
 
-        public void render(Graphics g) {
+        private void render(Graphics g) {
             g.setColor(Color.BLACK);
             g.fillRect(15, 15, 200, 32);
             g.setColor(new Color(75, greenValue, 0));
@@ -87,8 +72,6 @@ public class Window extends Canvas {
             g.drawRect(15, 15, HEALTH*2, 32);
             g.drawString("Current Score: " + score, 15, 70);
             g.drawString("Current Level: " + level, 15, 90);
-
         }
-
     }
 }
