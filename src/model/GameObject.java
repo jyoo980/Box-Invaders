5e19d28d4a9d588de427dbae9a2febd56562a0f2
James Yoo <jyoo980@gmail.com>
2018-02-09 17:29:54 -0800

    Refactored GameObject.java

--- src/model/GameObject.java
+++ src/model/GameObject.java
@@ -2,23 +2,23 @@ package model;
 
 import java.awt.*;
 
-/**
- * Created by James on 2016-12-28.
- * Basically, this is an ABSTRACT class because it refers to ALL of our
- * possible GameObjects i.e. enemies, heroes, items, players etc...
- */
 public abstract class GameObject {
 
-    public float x,y;          // this is the location of each GameObject
-    protected ID id;            // this represents the ID of the object, since it can be either a player or an enemy
-    protected float velX, velY;   // this represents the x and y speed(s) of a given GameObject
+    protected float x,y;
+    protected ID id;
+    protected float velX, velY;
 
     public GameObject(float x, float y, ID id) {
         this.x = x;
         this.y = y;
         this.id = id;
-
     }
+
+    public abstract void tick();
+
+    public abstract Rectangle getBounds();
+
+    public abstract void render(Graphics g);
 
     public void setX(int x) {
         this.x = x;
@@ -28,7 +28,9 @@ public abstract class GameObject {
         this.y = y;
     }
 
-    public void setID(ID id) { this.id = id; }
+    public void setID(ID id) {
+        this.id = id;
+    }
 
     public void setvelX(int velx) {
         this.velX = velx;
@@ -39,33 +41,22 @@ public abstract class GameObject {
     }
 
     public float getX() {
-        return this.x;
+        return x;
     }
 
     public float getY() {
-        return this.y;
+        return y;
     }
 
     public float getvelX() {
-        return this.velX;
+        return velX;
     }
 
     public float getVelY() {
-        return this.velY;
+        return velY;
     }
 
-    public ID getID() { return this.id; }
-
-    public abstract void tick();
-
-    public abstract Rectangle getBounds();
-
-
-    public abstract void render(Graphics g);
-
-
-
-
-
-
+    public ID getID() {
+        return id;
+    }
 }
--- src/model/ID.java
+++ src/model/ID.java
@@ -1,13 +1,9 @@
 package model;
 
-/**
- * Created by James on 2016-12-28.
- */
 public enum ID {
     Player(),
     Trail(),
     FastEnemy(),
     Enemy(),
-    SmartEnemy();
-
+    SmartEnemy()
 }
