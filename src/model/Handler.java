ab41f134363adb9e24e229ae0be2e2cb99b95b87
James Yoo <jyoo980@gmail.com>
2018-02-09 17:40:45 -0800

    Refacted Handler.java

--- src/model/Handler.java
+++ src/model/Handler.java
@@ -3,52 +3,27 @@ package model;
 import java.awt.*;
 import java.util.LinkedList;
 
-/**
- * Created by James on 2016-12-28.
- * This updates all our objects that we will use in this game
- *
- */
 public class Handler {
-    public static LinkedList<GameObject> object = new LinkedList<>();     //this is the field which holds all the objects in game
 
+    private static LinkedList<GameObject> gameObjects = new LinkedList<>();
 
-    /**
-     * Updates all the objects within the object LinkedList
-     *
-     */
     public void tick() {
-        for (int j = 0; j < object.size(); j++) {
-            GameObject tempObject = object.get(j);      //we temporarily grab an object from the list
-
-            tempObject.tick();      //invoke the GameObject tick method on the object
-
+        for (GameObject gameObj : gameObjects) {
+            gameObj.tick();
         }
     }
 
-    /**
-     * Renders all the GameObjects within the LinkedList object
-     * @param g
-     */
     public void render(Graphics g) {
-        for (int j = 0; j < object.size(); j++) {
-            GameObject tempObject = object.get(j);
-
-            tempObject.render(g);
+        for (GameObject gameObj : gameObjects) {
+            gameObj.render(g);
         }
-
     }
 
-
-    public void addObject(GameObject gO) {
-        object.add(gO);
+    public void addObject(GameObject gameObj) {
+        gameObjects.add(gameObj);
     }
 
-    public void removeObject(GameObject gO) {
-        object.remove(gO);
-
+    public void removeObject(GameObject gameObj) {
+        gameObjects.remove(gameObj);
     }
-
-
-
-
 }
