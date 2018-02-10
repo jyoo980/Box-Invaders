eabe9481d81db187a597c0e8a4f67606e72e3ae0
James Yoo <jyoo980@gmail.com>
2018-02-10 13:10:11 -0800

    Started refactor of Window.java

--- src/ui/Window.java
+++ src/ui/Window.java
@@ -1,41 +1,38 @@
 package ui;
 
-/**
- * Created by: James Yoo
- *
- */
-
 import javax.swing.*;
 import java.awt.*;
 
-import static javax.swing.JFrame.EXIT_ON_CLOSE;
-
 public class Window extends Canvas {
-
     private static final long serialVersionUID = -2408460053372835L;
 
-    public Window(int width, int height, String title, Game game) {
+    private Window(int width, int height, String title, Game game) {
         JFrame window = new JFrame(title);      //basically the frame of our game
+        setWindowDimensions(window, width, height);
+        setWindowBehaviour(window);
 
-        window.setPreferredSize(new Dimension(width, height));  //the preferred size our window will be
-        window.setMaximumSize(new Dimension(width, height));    //the maximum size ...
-        window.setMinimumSize(new Dimension(width, height));    //the minimum size ...
-
-        window.setDefaultCloseOperation(EXIT_ON_CLOSE);     //this confirms the window will close when we exit
-        window.setResizable(false);             //user cannot resize the window
-        window.setLocationRelativeTo(null);     //default setting, makes window appear in centre of screen
-
-        window.add(game);
-        window.setVisible(true);    //this makes the window visible
-        game.start();
-
-
-
+        addWindowStartGame(window, game);
     }
 
-    /**
-     * Created by James on 2016-12-28.
-     */
+    private void setWindowDimensions(JFrame w, int width, int height) {
+        Dimension defaultDimension = new Dimension(width, height);
+        w.setPreferredSize(defaultDimension);
+        w.setMaximumSize(defaultDimension);
+        w.setMinimumSize(defaultDimension);
+    }
+
+    private void setWindowBehaviour(JFrame w) {
+        w.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        w.setResizable(false);
+        w.setLocationRelativeTo(null);
+    }
+
+    private void addWindowStartGame(JFrame w, Game game) {
+        w.add(game);
+        w.setVisible(true);
+        game.start();
+    }
+
 
     public static class HeadUpDisplay {
         public static int HEALTH = 100;     //this is the initial health that we start off with
